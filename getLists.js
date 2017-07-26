5d40478638b0ce2d80d2de51d9f119d56d2f140c
mazzcris <cristiano.mazzarini@gmail.com>
2017-07-26 18:00:47 +0200

    Patch list link creation

--- getLists.js
+++ getLists.js
@@ -1,32 +1,43 @@
 function setButtons () {
   var lists = document.getElementsByClassName('list');
-  var boardId = window.location.pathname.replace('/b/', '');
-  boardId = boardId.substring(0, boardId.lastIndexOf("/"));
 
   for (var i = 0; i < lists.length; i++) {
     var list = lists[i];
 
-    if (listNotEmpty(lists[i]) && !listHasButton(list)) {
-      var title = list.getElementsByClassName('list-header-name-assist')[0];
-
-      var oneCard = list.querySelectorAll('a.list-card')[0];
-      var oneCardHref = oneCard.href;
-      var oneCardUrl = oneCardHref.replace("https://trello.com/c/", "");
-      var oneCardId = oneCardUrl.replace(/\/(.*)/g, "")
-
-      var newElement = '<a class="list-header-extras-menu dark-hover sortello-link" title="Sort cards with Sortello" target="_blank" href="http://sortello.ideato.it/?extId=' + oneCardId + '">' +
-          '<span class="icon-sm" style="background: url(' + chrome.runtime.getURL('icon.png') + '); background-size: contain;">' +
-          '</span>' +
-          '</a>';
-      var extras = list.getElementsByClassName('list-header-extras')[0];
-      extras.innerHTML = newElement + extras.innerHTML;
+    if (listNotEmpty(list) && !listHasButton(list)) {
+      addButton(list);
     }
 
-    if (!listNotEmpty(lists[i]) && listHasButton(list)) {
-      var toRemove = list.getElementsByClassName('sortello-link')[0];
-      toRemove.parentNode.removeChild(toRemove);
+    if (!listNotEmpty(list) && listHasButton(list)) {
+      removeButton(list);
+    }
+
+    if(listNotEmpty(list) && !listHasButton(list) && !buttonIsComplete(list)){
+      removeButton(list);
+      addButton(list);
     }
   }
+}
+
+function addButton(list){
+  var title = list.getElementsByClassName('list-header-name-assist')[0];
+
+  var oneCard = list.querySelectorAll('a.list-card')[0];
+  var oneCardHref = oneCard.href;
+  var oneCardUrl = oneCardHref.replace("https://trello.com/c/", "");
+  var oneCardId = oneCardUrl.replace(/\/(.*)/g, "")
+
+  var newElement = '<a class="list-header-extras-menu dark-hover sortello-link" title="Sort cards with Sortello" target="_blank" href="http://sortello.ideato.it/?extId=' + oneCardId + '">' +
+      '<span class="icon-sm" style="background: url(' + chrome.runtime.getURL('icon.png') + '); background-size: contain;">' +
+      '</span>' +
+      '</a>';
+  var extras = list.getElementsByClassName('list-header-extras')[0];
+  extras.innerHTML = newElement + extras.innerHTML;
+}
+
+function removeButton(list){
+  var toRemove = list.getElementsByClassName('sortello-link')[0];
+  toRemove.parentNode.removeChild(toRemove);
 }
 
 function listNotEmpty (list) {
@@ -35,6 +46,15 @@ function listNotEmpty (list) {
 
 function listHasButton (list) {
   return list.getElementsByClassName('sortello-link').length > 0;
+}
+
+function buttonIsComplete(list){
+  var suffix = 'extId=';
+  var sortelloLink = list.getElementsByClassName('sortello-link');
+  if(sortelloLink.href === undefined){
+    return false
+  }
+  return sortelloLink.href.indexOf(suffix, this.length - suffix.length) !== -1;
 }
 
 setButtons();
